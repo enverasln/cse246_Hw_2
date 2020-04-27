4e6a201b38c6ede8b6b16fa7a0e450d94b3d6836
enver.asln <enver.asln@gmail.com>
2020-04-27 17:38:53 +0300

    InputUtility class was created to generate input

---
+++ src/InputUtility.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class InputUtility {
+
+    public static int[] generateRandomInput(int inputSize) {
+        Random rnd = new Random();
+        int[] input = new int[inputSize];
+        for(int i = 0; i < inputSize; i++) {
+            input[i] = rnd.nextInt(500);
+        }
+        return input;
+    }
+
+    public static int[] generateReverseOrderedInput(int inputSize) {
+        int[] input = generateOrderedInput(inputSize);
+        int temp;
+        for(int i = 0; i < Math.floor(inputSize/2.0); i++) {
+            temp = input[i];
+            input[i] = input[inputSize-i-1];
+            input[inputSize-i-1] = temp;
+        }
+        return input;
+    }
+
+    public static int[] generateOrderedInput(int inputSize) {
+        int[] input = generateRandomInput(inputSize);
+        Arrays.sort(input);
+        return input;
+    }
+
+    public static int[] generatePercentageOrderedInput(int inputSize, int percentage) {
+        int[] input = generateOrderedInput(inputSize);
+        int tempSize = inputSize * percentage/100;
+        Random rnd = new Random();
+        int j;
+        for (int i = 0; i < tempSize; i++) {
+            j = tempSize + rnd.nextInt(tempSize);
+            input[tempSize+i]=input[j];
+        }
+        return input;
+    }
+
+}
