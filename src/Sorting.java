aa1eabb9d9bdd6134d590359a3c465dc57576300
enver.asln <enver.asln@gmail.com>
2020-04-28 03:48:29 +0300

    Abstract Sorting class was created.

---
+++ src/Sorting.java
@@ -0,0 +1,44 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public abstract class Sorting {
+    private long count;
+    private long exchange;
+
+    public abstract int sort(int[] input);
+
+    public void  write(String fileName, String str) {
+        File file = new File(fileName);
+        FileWriter fw;
+        BufferedWriter bw;
+
+        try {
+            if(!file.exists()) {
+                file.createNewFile();
+            }
+            fw = new FileWriter(file, true);
+            bw = new BufferedWriter(fw);
+            bw.write(str);
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void increaseCount() {
+        count++;
+    }
+
+    public long getCount() {
+        count = count + exchange;
+        exchange = 0;
+        return count;
+    }
+
+    public void increaseExchange() {
+        exchange++;
+    }
+}
